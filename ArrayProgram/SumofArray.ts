cfaf1d54fb8844e689bc8a040a7d78f018529b14
TRKQA23 <tanmayqaa2023@gmail.com>
2025-04-24 20:37:03 +0530

    TypeScript Program

--- ArrayProgram/SumofArray.ts
+++ ArrayProgram/SumofArray.ts
@@ -1,16 +1,15 @@
-function sumArray(numbers :number[]): number
-{
-let total=0;
- for (let num of  numbers)
- {
-    total += num;
- }
- return total;
+function sumArray(numbers: number[]): number {
+    let total = 0;
+    for (let num of numbers) {
+        total += num;
+    }
+    return total;
 
 
 }
 
-const numberss=[20,30,40,50,60];
+
+const numberss = [20, 30, 40, 50, 60];
 
 console.log(`The array is : [${numberss}]`);
 
